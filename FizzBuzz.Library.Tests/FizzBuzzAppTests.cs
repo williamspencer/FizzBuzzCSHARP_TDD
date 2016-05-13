d4445ec736d5bec05e313f01cb1eaff9e8353d67
williamspencer <badgley@gmail.com>
2016-05-13 10:54:32 -0400

    added failing test 5

--- FizzBuzz.Library.Tests/FizzBuzzAppTests.cs
+++ FizzBuzz.Library.Tests/FizzBuzzAppTests.cs
@@ -23,5 +23,11 @@ namespace FizzBuzz.Library.Tests
             string output = FizzBuzzApp.GetValue(3);
             Assert.AreEqual("Fizz", output);
         }
+        [Test]
+        public void BuzzApp_When5_ReturnsBuzz()
+        {
+            string output = FizzBuzzApp.GetValue(5);
+            Assert.AreEqual("Buzz", output);
+        }
     }
 }
