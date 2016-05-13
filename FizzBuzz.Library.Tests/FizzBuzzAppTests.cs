b1d98fc70715aabe522b1258022fda4a9905388b
williamspencer <badgley@gmail.com>
2016-05-13 10:29:58 -0400

    initial commit, test shell setup

---
+++ FizzBuzz.Library.Tests/FizzBuzzAppTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzAppTests
+    {
+        [Test]
+        public void BuzzApp_When1_Return1()
+        {
+           string output = FizzBuzzApp.GetValue(1);
+        }
+    }
+}
---
+++ FizzBuzz.Library/FizzBuzzApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzApp
+    {
+        public static string GetValue(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
